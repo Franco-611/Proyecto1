73cddc54c77c3acaf1ead973f5dd6ffaa3bba1fa
franco611 <62269078+franco611@users.noreply.github.com>
2022-05-26 21:56:26 -0600

    createRoot

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <App />,
-  document.getElementById('root'),
-
 )
+
 /*
 const container = document.getElementById('root')
 hydrateRoot(container, <App />)
